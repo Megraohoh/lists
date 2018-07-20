45bf56eadbe2ff791717e1bf699a4d3af17d2cf5
Meghan Debity <mdebity@gmail.com>
2018-07-20 13:12:54 -0500

    completed first instruction

--- random_swuared.py
+++ random_swuared.py
@@ -1,8 +1,7 @@
 # 1. Using the `random` module and the `range` method, generate a list of 20 random numbers between 0 and 49.
-#     ```
-#     import random
+import random
 
-#     random_numbers = [...insert awesome code here...]
-#     print(random_numbers)
-#     ```
+random_numbers = random.sample(range(50), k=20)
+print(random_numbers)
+
 # 2. With the resulting list, use a list comprehension to build a new list that contains each number squared. For example, if the original list is `[2, 5]`, the final list will be `[4, 25]`.
